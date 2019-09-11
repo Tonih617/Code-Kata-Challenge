91a022601e9a3c7b8fa2307907b54de7622d0218
Toni <Tonih617@gmail.com>
2019-09-11 12:28:33 -0400

    update
    added another image for if the user does not match the phrases, also added psuedocode to explain the process.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-function checkMatchingStrings(stringOne, stringTwo) {
-  if (stringOne.localeCompare(stringTwo) === 0) {
-    $('.answer').text("Congratulations its a match!")
-    var img = document.createElement("img");
-    img.src = "public/Sunny-day-photo.png";
-    var src = document.getElementById("Sunny-Photo");
+function checkMatchingStrings(stringOne, stringTwo) {//creating a function to match both strings of phrases//
+  if (stringOne.localeCompare(stringTwo) === 0) {// using a local compare function to calculate and compare both strings//
+    $('.answer').text("Congratulations its a match!")//adding a congratulations message to the form if phrases are a match//
+    var img = document.createElement("img");//creating an image varible//
+    img.src = "public/Sunny-day-photo.png";//adding the image src//
+    var src = document.getElementById("Sunny-Photo");//getting image from the DOM by ID//
     img.className ="w3-spin";
-    src.appendChild(img);
-  } else {
-    $('.answer').text("Sorry, it's not a match please try again!")
+    src.appendChild(img);//appending the child element of the img src to the DOM//
+  } else {//else statement for if user inputs the wrong phrases//
+    $('.answer').text("Sorry, it's not a match please try again!")//adding a sorry its not a match text if user inputs the wrong phrases//
+    var img = document.createElement("img");//creating the image varible//
+    img.src = "public/Scratching-head-emoji.png";//adding image src//
+    var src = document.getElementById("Scratching-head-emoji");//getting image from the DOM by ID//
+    src.appendChild(img);//appending image to the DOM//
   }
 }
 
-$('form').submit(function(event) {
+$('form').submit(function(event) {//creating a prevent default function on the submit button to allow the the page to submit and return a message//
   event.preventDefault();
-  let phraseOne = $('#phrase-one').val().toLowerCase();
-  let phraseTwo = $('#phrase-two').val().toLowerCase();
+  let phraseOne = $('#phrase-one').val().toLowerCase();//allowing the phrases to not be case sensitive for phrase one//
+  let phraseTwo = $('#phrase-two').val().toLowerCase();//allowing the phrases to not be case sensitive for phrase two//
 
-  checkMatchingStrings(phraseOne, phraseTwo)
+  checkMatchingStrings(phraseOne, phraseTwo)//checking to make sure both strings of phrases are a match upon submit//
 });
+
+
+
+
+
+
+
+
+
+
 
 // $('#clear-button').click(function(event) {
 //   $('input[name="phrase-one"]').val("");
